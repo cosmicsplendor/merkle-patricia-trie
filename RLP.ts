78fa16cf42bd599917ea3a99953e1bd0f49e024e
cosmicsplendor <ufocalypse@gmail.com>
2026-07-07 18:01:14 +0545

    Write RLP skeleton

---
+++ RLP.ts
@@ -0,0 +1,9 @@
+type EncodedData = string;
+type DecodedData = string | DecodedData[];
+
+export const encode = (data: DecodedData): EncodedData => {
+    return ""
+}
+export const decode = (data: EncodedData): DecodedData => {
+    return ""
+}
